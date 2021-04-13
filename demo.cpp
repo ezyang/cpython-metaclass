9e7377e7178c0b58c85a5be616a3f892197b6400
Edward Z. Yang <ezyang@fb.com>
2021-04-13 10:16:03 -0700

    bugfix
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- demo.cpp
+++ demo.cpp
@@ -35,7 +35,7 @@ static PyTypeObject foometa_type = {
     0,                                          /* tp_getattro */
     0,                                          /* tp_setattro */
     0,                                          /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HAVE_GC, /* tp_flags */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
     0,                                          /* tp_doc */
     0,                                          /* tp_traverse */
     0,                                          /* tp_clear */
